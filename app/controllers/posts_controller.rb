d2d5cdb7a7edeabdac405b7ebf775bd953e97edb
kaiyukun <kaiyu.s0116@gmail.com>
2021-08-28 15:15:14 +0900

    create crud

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,45 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    post = Post.new(post_params)
+    if post.save
+      redirect_to posts_path
+    else
+      redirect_to new_post_path
+    end
   end
 
   def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(paraml[:id])
+  end
+  
+  def update
+    post = Post.find(params[:id])
+    if post.save
+      redirect_to posts_path
+    else
+      redirect_to new_post_path
+    end
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:theme, :content, :image, :link)
   end
 end
