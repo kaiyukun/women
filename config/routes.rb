e89dbecd5349c7d9b46acb026f274b549345e750
kaiyukun <kaiyu.s0116@gmail.com>
2021-09-03 01:09:30 +0900

    簡単な検索機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :posts do 
     collection do
+      get :main
       get :search
     end
   end
-  root 'posts#search'
+  root 'posts#main'
 end
