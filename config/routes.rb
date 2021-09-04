c266a34d3ce9b8e3a157e4e6a64095f7e4097188
kaiyukun <kaiyu.s0116@gmail.com>
2021-09-04 11:16:05 +0900

    Dmの前半

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'rooms/index'
+  get 'rooms/show'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :posts do 
@@ -9,5 +11,7 @@ Rails.application.routes.draw do
     resources :likes, only: [:create, :destroy]
     resources :comments, only: [:create]
   end
+  resources :messages, :only => [:create]
+  resources :rooms, :only => [:create, :show]
   root 'posts#main'
 end
